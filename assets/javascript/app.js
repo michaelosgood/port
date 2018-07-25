491fdb4b4f8d3bab150d350daa323488cc3d75e6
Michael Osgood <michael.f.osgood@gmail.com>
2018-07-24 21:56:15 -0700

    updated navbar app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,9 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("lines").style.opacity = "0.01";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("lines").style.opacity = "1.0";
 }
